03d8a016fe12b66f42b3cd68d9e7288b9f1fd3b1
junho <junho6667@gmail.com>
2021-06-05 18:23:51 +0900

    2021/06/05

--- src/main/java/hello/hellospring/controller/domain/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/controller/domain/repository/MemoryMemberRepository.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long,Member> store = new HashMap<>();
-    private static long sequence = 0L
+    private static long sequence = 0L;
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
@@ -22,7 +22,7 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Optional<Member> findByName(String name) {
-        store.values().stream()
+        return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
@@ -30,5 +30,9 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
