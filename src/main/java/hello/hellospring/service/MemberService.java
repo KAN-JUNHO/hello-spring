a83b4b09048ad33881f741dafe9eef25e50b819e
junho <junho6667@gmail.com>
2021-06-06 14:40:39 +0900

    2021/06/06

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,14 +2,20 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.domain.repository.MemberRepository;
-import hello.hellospring.domain.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     public Long join(Member member){
         validateDuplicateMember(member);//중복확인
@@ -20,7 +26,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원");
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
     //전체조회
